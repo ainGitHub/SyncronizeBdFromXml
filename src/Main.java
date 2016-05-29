2f549440e414bae4ec35604d50913969b9cbb6e2
Ainur <ainurmullin@mail.ru>
2016-05-29 15:23:34 +0300

    add javadoc for main

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,9 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Главный класс, точка входа
+ */
 public class Main {
     private static final String SYNC_CODE = "sync";
     private static final String UPLOAD_CODE = "upload";
@@ -35,28 +38,43 @@ public class Main {
         }
     }
 
+
+    /**
+     * Метод для выгрузки данных из бд в xml файл
+     *
+     * @param fileName - название xml файла, в который будет выгружены данные
+     */
     public static void uploadDB(String fileName) {
         DepartmentDao departmentDao = new DepartmentDaoImpl();
         try {
+            rootLogger.info("Начало выгрузки данных в xml");
             List<Department> departments = departmentDao.findAll();
 
             XmlWriter xmlReader = new XmlWriter();
             xmlReader.writeToFile(fileName, departments);
-            rootLogger.info("Uploaded");
+            rootLogger.info("Выгрузка завершена");
         } catch (DaoException e) {
-            rootLogger.info("Can't upload");
+            rootLogger.info("Не возможно выгрузить");
             e.printStackTrace();
         }
     }
 
+
+    /**
+     * Метод для синхронизации данных xml файла и бд
+     * @param fileName - название xml файла, с которого будут считаны данные
+     */
     public static void syncDB(String fileName) {
         XmlReader xmlReader = new XmlReader();
         try {
+            rootLogger.info("Начало синхронизации");
+
             Set<Department> xmlDepartments = xmlReader.parseXML(fileName);
 
             SynchronizedService synchronizedService = new SynchronizedService();
             synchronizedService.synchronize(xmlDepartments);
-            rootLogger.info("Synchronized");
+
+            rootLogger.info("Конец синхронизации");
         } catch (NotUniqueElementException e) {
             rootLogger.error("В файле существует два объекта с одинаковыми натуральными ключами");
         } catch (SQLException e) {
